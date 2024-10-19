dfb54cff2d9a70621ea295dc68263ebb655a3f7e
sandman <bmsandoval@gmail.com>
2024-10-18 17:30:01 -0700

    bork: stubbing out initial service

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # counter
-golang successor to "DCSB", but focused more on the counter (death or otherwise)
+
+Counter (badly named) will be a golang-based successor to "DCSB", but focused more on the counter (death or otherwise)
+
+
+## Project Plan
+
+### Problem:
+
+* DCSB doesn't work on MacOS
+* OBS text display accepts a constant string, or pulling from a file
+
+### Goal:
+
+create an app that clones the 'counter' feature of DCSB to allow a global hotkey for incrementing a counter
+
+### Requirements:
+
+* Read from/Write to file
+* Global Hotkeys
+* Possible to manage file and hotkeys
+  * TUI/GUI?
+  * For quick POC, could be passed in as flags
---
+++ src/state.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bmsandoval/counter/pkg"
+	"os"
+	"path/filepath"
+)
+
+// ModifyCounter modifies the counter (increment or decrement)
+func ModifyCounter(delta int) error {
+	filePath, err := GetCounterFilePath()
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return err
+	}
+
+	// Open and read the current value from the file
+	file, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return err
+	}
+
+	// Convert the content to an integer
+	var counter int
+	_, err = fmt.Sscanf(string(file), "%d", &counter)
+	if err != nil {
+		return err
+	}
+
+	// Modify the counter
+	counter += delta
+
+	// Write the updated counter back to the file
+	err = os.WriteFile(filePath, []byte(fmt.Sprintf("%d", counter)), 0644)
+	if err != nil {
+		fmt.Println("Error writing file:", err)
+		return err
+	}
+	fmt.Printf("Counter updated to: %d\n", counter)
+	return nil
+}
+
+// GetCounterFilePath gets the path of the counter file
+func GetCounterFilePath() (string, error) {
+	// TODO : remove hardcoded file path
+	home, err := pkg.HomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(home, "counter.txt"), nil
+}
